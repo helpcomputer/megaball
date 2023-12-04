122d74543b6b142b871534698bd164bf747d029e
Takashi Kitao <takashi.kitao@gmail.com>
2023-12-05 08:12:47 +0900

    Adapted to Pyxel 2.0

--- megaball/main.py
+++ megaball/main.py
@@ -1,21 +1,21 @@
 
+import constants
+import game
+import input
 import pyxel
 
-import constants
-import input
-import game
 
 class App:
     def __init__(self):
         pyxel.init(
-            constants.GAME_WIDTH, 
-            constants.GAME_HEIGHT, 
-            title=constants.GAME_TITLE, 
-            fps=constants.GAME_FPS, 
+            constants.GAME_WIDTH,
+            constants.GAME_HEIGHT,
+            title=constants.GAME_TITLE,
+            fps=constants.GAME_FPS,
         )
-        
+
         pyxel.load(constants.RESOURCE_FILE)
-        pyxel.image(0).load(0, 0, constants.IMAGE_BANK_0_FILE)
+        pyxel.images[0].load(0, 0, constants.IMAGE_BANK_0_FILE)
 
         self.input = input.Input()
         self.game = game.Game()
@@ -27,9 +27,8 @@ class App:
     def update(self):
         self.input.get()
         self.game.update(self.input)
-        
+
     def draw(self):
         self.game.draw()
 
 App()
-  
